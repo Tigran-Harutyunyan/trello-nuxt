1e7c3f9f02e16f2759fe8bfbe4ae393cbd4b4ca9
Tigran <harutyunyan.tigran1975@gmail.com>
2024-02-02 15:00:41 +0400

    created skeleton for too be used when clerk is loading

---
+++ components/dashboard/NavItemSkeleton.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { Skeleton } from "@/components/ui/skeleton";
+</script>
+
+<template>
+  <div class="flex items-center gap-x-2">
+    <div class="w-10 h-10 relative shrink-0">
+      <Skeleton class="h-full w-full absolute" />
+    </div>
+    <Skeleton class="h-10 w-full" />
+  </div>
+</template>
