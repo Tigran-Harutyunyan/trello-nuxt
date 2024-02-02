abd9705328430b93df0e4baf4f9a7eddfc048fc4
Tigran <harutyunyan.tigran1975@gmail.com>
2024-02-02 14:54:20 +0400

    created organization page layout

---
+++ layouts/organization.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import Navbar from "@/components/dashboard/Navbar.vue";
+import Sidebar from "@/components/dashboard/Sidebar.vue";
+</script>
+<template>
+  <div class="h-full">
+    <Navbar />
+    <main class="pt-20 md:pt-24 px-4 max-w-6xl 2xl:max-w-screen-xl mx-auto">
+      <div class="flex gap-x-7">
+        <div class="w-64 shrink-0 hidden md:block">
+          <ClientOnly>
+            <Sidebar />
+          </ClientOnly>
+        </div>
+        <NuxtPage>
+          <slot />
+        </NuxtPage>
+      </div>
+    </main>
+  </div>
+</template>
