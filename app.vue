a56ce7ade191c4dd16253d41ef75a84e1e9497e3
Tigran <harutyunyan.tigran1975@gmail.com>
2024-02-02 14:57:46 +0400

    changed app.vue

---
+++ app.vue
@@ -0,0 +1,10 @@
+<template>
+  <NuxtLoadingIndicator color="#f43f5e" />
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
+  <Toaster />
+</template>
+<script setup lang="ts">
+import Toaster from "@/components/ui/toast/Toaster.vue";
+</script>
