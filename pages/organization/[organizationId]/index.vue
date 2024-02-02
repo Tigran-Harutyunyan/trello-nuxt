f617ff59a749e639a246036cc0d8a16fd948907d
Tigran <harutyunyan.tigran1975@gmail.com>
2024-02-02 14:53:09 +0400

    added initial organization page

---
+++ pages/organization/[organizationId]/index.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: "organization",
+  middleware: "auth",
+});
+</script>
+
+<template>
+  <NuxtPage />
+
+  <!-- <div className="w-full mb-20">
+      <Info isPro={isPro} />
+      <Separator className="my-4" />
+      <div className="px-2 md:px-4">
+        <Suspense fallback="<BoardList.Skeleton />">
+          <BoardList />
+        </Suspense>
+      </div>
+    </div> -->
+
+  <!-- <OrganizationSwitcher
+    hidePersonal
+    :appearance="appearance"
+    :afterCreateOrganizationUrl="(org) => `/organization/${org.id}`"
+    :afterSelectOrganizationUrl="(org) => `/organization/${org.id}`"
+    afterLeaveOrganizationUrl="/select-org"
+  /> -->
+</template>
