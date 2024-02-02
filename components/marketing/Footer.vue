8bf04f2dd700fc13d1952a807cee7ed823665e2e
Tigran <harutyunyan.tigran1975@gmail.com>
2024-02-02 14:46:45 +0400

    created header and footer components for marketing page

---
+++ components/marketing/Footer.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { Button } from "@/components/ui/button";
+import Logo from "@/components/Logo.vue";
+</script>
+
+<template>
+  <div class="fixed bottom-0 w-full p-4 border-t bg-slate-100">
+    <div
+      class="md:max-w-screen-2xl mx-auto flex items-center w-full justify-between"
+    >
+      <Logo />
+      <div
+        class="space-x-4 md:block md:w-auto flex items-center justify-between w-full"
+      >
+        <Button size="sm" variant="ghost"> Privacy Policy </Button>
+        <Button size="sm" variant="ghost"> Terms of Service </Button>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ components/marketing/Navbar.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import Logo from "@/components/Logo.vue";
+import { Button } from "@/components/ui/button";
+</script>
+
+<template>
+  <div
+    class="fixed top-0 w-full h-14 px-4 border-b shadow-sm bg-white flex items-center"
+  >
+    <div
+      class="md:max-w-screen-2xl mx-auto flex items-center w-full justify-between"
+    >
+      <Logo />
+      <div
+        class="space-x-4 md:block md:w-auto flex items-center justify-between w-full"
+      >
+        <Button size="sm" variant="outline" as-child>
+          <NuxtLink to="/sign-in"> Login </NuxtLink>
+        </Button>
+        <Button size="sm" as-child>
+          <NuxtLink to="/sign-up"> Get Taskify for free </NuxtLink>
+        </Button>
+      </div>
+    </div>
+  </div>
+</template>
