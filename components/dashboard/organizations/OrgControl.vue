1720ea398cc9907214b5524897d480ecc188d673
Tigran <harutyunyan.tigran1975@gmail.com>
2024-02-02 14:55:56 +0400

    added Organization controller to be able watch url changes

---
+++ components/dashboard/organizations/OrgControl.vue
@@ -0,0 +1,33 @@
+<template></template>
+<script setup lang="ts">
+// TODO: switch organization on url paste
+
+//   import { useClerk, useClerkProvide, useAuth } from "vue-clerk";
+
+//   const { derivedState } = useClerkProvide();
+
+//   const { setActive, getOrganizationMemberships, organization } = useClerk();
+//   const { isLoaded } = useAuth();
+
+//   const userMemberships = await getOrganizationMemberships();
+
+//   const route = useRoute();
+
+// watchEffect(() => {
+//   if (isLoaded) {
+//     if (organization?.id !== route.params?.organizationId) {
+//       const isValidOrgId = userMemberships.some(
+//         (mem) => mem.organization.id === route.params?.organizationId
+//       );
+//       console.log(derivedState?.value?.orgId);
+//       console.log(isValidOrgId);
+
+//       if (isValidOrgId) {
+//         setActive({
+//           organization: route.params?.organizationId as string,
+//         });
+//       }
+//     }
+//   }
+// });
+</script>
