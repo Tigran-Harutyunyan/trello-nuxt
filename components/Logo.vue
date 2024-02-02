e490526a6ee8a4f71968ae0c4c4d55c2051cea81
Tigran <harutyunyan.tigran1975@gmail.com>
2024-02-02 14:57:15 +0400

    added Logo

---
+++ components/Logo.vue
@@ -0,0 +1,10 @@
+<template>
+  <NuxtLink to="/">
+    <div
+      class="hover:opacity-75 transition items-center gap-x-2 hidden md:flex"
+    >
+      <NuxtImg src="/logo.svg" alt="Logo" height="30" width="30" />
+      <p class="text-lg text-neutral-700 pb-1 font-local">Taskify</p>
+    </div>
+  </NuxtLink>
+</template>
