06cd191c9b6bef6bc82a1ee113d8da2ac7aae80d
Tigran <harutyunyan.tigran1975@gmail.com>
2024-02-02 14:48:22 +0400

    created default layout

---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { ClerkLoaded, ClerkLoading } from "vue-clerk";
+import Loading from "~/components/auth/Loading.vue";
+</script>
+<template>
+  <ClerkLoading>
+    <Loading />
+  </ClerkLoading>
+  <ClerkLoaded>
+    <slot />
+  </ClerkLoaded>
+</template>
