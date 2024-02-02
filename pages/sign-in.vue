099c2f5a2e58c6b65861c48f177683f3bd9c0f96
Tigran <harutyunyan.tigran1975@gmail.com>
2024-02-02 14:54:02 +0400

    created sign-in and sign-out pages wth clerk components

---
+++ pages/sign-in.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { SignIn } from "vue-clerk";
+
+definePageMeta({ layout: "auth", isPublicRoute: true });
+</script>
+
+<template>
+  <div class="grid h-full place-items-center">
+    <SignIn sign-up-url="/sign-up" />
+  </div>
+</template>
---
+++ pages/sign-up.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { SignUp } from "vue-clerk";
+definePageMeta({ layout: "auth", isPublicRoute: true });
+</script>
+
+<template>
+  <div class="grid h-full place-items-center">
+    <SignUp />
+  </div>
+</template>
