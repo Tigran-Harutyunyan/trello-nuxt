eea2c0b0525ad2fd04abceb378daef9e83f08180
Tigran <harutyunyan.tigran1975@gmail.com>
2024-02-02 14:45:29 +0400

    added select-org page to trigger organization switch

---
+++ pages/select-org.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+definePageMeta({ layout: "auth", middleware: "auth" });
+
+import { OrganizationSwitcher } from "vue-clerk";
+
+const appearance = {
+  elements: {
+    rootBox: {
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      width: "100%",
+    },
+    organizationSwitcherTrigger: {
+      padding: "6px",
+      width: "100%",
+      borderRadius: "8px",
+      border: "1px solid #E5E7EB",
+      justifyContent: "space-between",
+      backgroundColor: "white",
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="grid h-full place-items-center">
+    <OrganizationSwitcher
+      hidePersonal
+      :appearance="appearance"
+      :afterCreateOrganizationUrl="(org) => `/organization/${org.id}`"
+      :afterSelectOrganizationUrl="(org) => `/organization/${org.id}`"
+    />
+  </div>
+</template>
