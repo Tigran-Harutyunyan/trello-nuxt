58880a3e85e0545d190a18a5bcbf512e6fabd64a
Tigran <harutyunyan.tigran1975@gmail.com>
2024-02-02 14:56:53 +0400

    dashboard layout

---
+++ layouts/dashboard.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import Navbar from "@/components/dashboard/Navbar.vue";
+</script>
+
+<template>
+  <div class="h-full">
+    <Navbar />
+    <NuxtPage>
+      <slot />
+    </NuxtPage>
+  </div>
+</template>
