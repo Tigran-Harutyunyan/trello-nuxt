f0feaff1b8dea00a609c065d1e08a5f46b1cb7b6
Tigran <harutyunyan.tigran1975@gmail.com>
2024-02-03 13:31:09 +0400

    changed the orgaization id page

--- pages/organization/[organizationId]/index.vue
+++ pages/organization/[organizationId]/index.vue
@@ -1,28 +1,24 @@
 <script setup lang="ts">
+import BoardList from "@/components/dashboard/organizations/BoardList.vue";
+import Info from "@/components/dashboard/organizations/Info.vue";
+import { Separator } from "@/components/ui/separator";
 definePageMeta({
   layout: "organization",
   middleware: "auth",
 });
+
+const isPro = false;
 </script>
 
 <template>
   <NuxtPage />
-
-  <!-- <div className="w-full mb-20">
-      <Info isPro={isPro} />
-      <Separator className="my-4" />
-      <div className="px-2 md:px-4">
-        <Suspense fallback="<BoardList.Skeleton />">
-          <BoardList />
-        </Suspense>
-      </div>
-    </div> -->
-
-  <!-- <OrganizationSwitcher
-    hidePersonal
-    :appearance="appearance"
-    :afterCreateOrganizationUrl="(org) => `/organization/${org.id}`"
-    :afterSelectOrganizationUrl="(org) => `/organization/${org.id}`"
-    afterLeaveOrganizationUrl="/select-org"
-  /> -->
+  <div class="w-full mb-20">
+    <Info :isPro="isPro" />
+    <Separator class="my-4" />
+    <div class="px-2 md:px-4">
+      <ClientOnly>
+        <BoardList />
+      </ClientOnly>
+    </div>
+  </div>
 </template>
