184b024ab8caf7bb38733d1698f9969b7585520a
Tigran <harutyunyan.tigran1975@gmail.com>
2024-02-03 13:32:38 +0400

    added skeleton compoonent

---
+++ components/dashboard/organizations/BoardListSkeleton.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { Skeleton } from "@/components/ui/skeleton";
+</script>
+
+<template>
+  <div class="grid gird-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
+    <Skeleton class="aspect-video h-full w-full p-2" />
+    <Skeleton class="aspect-video h-full w-full p-2" />
+    <Skeleton class="aspect-video h-full w-full p-2" />
+    <Skeleton class="aspect-video h-full w-full p-2" />
+    <Skeleton class="aspect-video h-full w-full p-2" />
+    <Skeleton class="aspect-video h-full w-full p-2" />
+    <Skeleton class="aspect-video h-full w-full p-2" />
+    <Skeleton class="aspect-video h-full w-full p-2" />
+  </div>
+</template>
