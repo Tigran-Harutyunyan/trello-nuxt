070ca740c091f4f9e9880bfb355fb8166cf5ce6f
Tigran <harutyunyan.tigran1975@gmail.com>
2024-02-03 13:27:06 +0400

    created a Hint component

---
+++ components/Hint.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+interface HintProps {
+  description: string;
+  side?: "left" | "right" | "top" | "bottom";
+  sideOffset?: number;
+}
+
+const {
+  description,
+  side = "bottom",
+  sideOffset = 0,
+} = defineProps<HintProps>();
+</script>
+
+<template>
+  <TooltipProvider>
+    <Tooltip :delayDuration="0">
+      <TooltipTrigger>
+        <slot />
+      </TooltipTrigger>
+      <TooltipContent
+        class="max-w-[220px] break-words"
+        :side="side"
+        :side-offset="sideOffset"
+      >
+        <p class="capitalize text-xs">
+          {{ description }}
+        </p>
+      </TooltipContent>
+    </Tooltip>
+  </TooltipProvider>
+</template>
