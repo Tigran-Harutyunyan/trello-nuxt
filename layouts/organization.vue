e25ad661cb88a160fca0d7f564f659cfaa2df64f
Tigran <harutyunyan.tigran1975@gmail.com>
2024-02-03 13:20:34 +0400

    added ClientOnly component

--- layouts/organization.vue
+++ layouts/organization.vue
@@ -8,9 +8,7 @@ import Sidebar from "@/components/dashboard/Sidebar.vue";
     <main class="pt-20 md:pt-24 px-4 max-w-6xl 2xl:max-w-screen-xl mx-auto">
       <div class="flex gap-x-7">
         <div class="w-64 shrink-0 hidden md:block">
-          <ClientOnly>
-            <Sidebar />
-          </ClientOnly>
+          <Sidebar />
         </div>
         <NuxtPage>
           <slot />
