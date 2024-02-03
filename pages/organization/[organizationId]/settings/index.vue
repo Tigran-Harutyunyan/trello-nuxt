fed035f10a4052a4dc85732d007969d3dd5c50f6
Tigran <harutyunyan.tigran1975@gmail.com>
2024-02-03 13:30:26 +0400

    added Organization  profile component to settings

--- pages/organization/[organizationId]/settings/index.vue
+++ pages/organization/[organizationId]/settings/index.vue
@@ -1,9 +1,25 @@
 <script setup lang="ts">
+import { OrganizationProfile } from "vue-clerk";
 definePageMeta({
   layout: "organization",
 });
+const appearance = {
+  elements: {
+    rootBox: {
+      boxShadow: "none",
+      width: "100%",
+    },
+    card: {
+      border: "1px solid #e5e5e5",
+      boxShadow: "none",
+      width: "100%",
+    },
+  },
+};
 </script>
 
 <template>
-  <div class="my-20"></div>
+  <div class="w-full">
+    <OrganizationProfile :appearance="appearance" />
+  </div>
 </template>
