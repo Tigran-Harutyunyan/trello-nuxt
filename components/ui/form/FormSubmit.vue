0b6b72931ba7f69c5d538d580f0ba653860c31b4
Tigran <harutyunyan.tigran1975@gmail.com>
2024-02-03 22:26:44 +0400

    added form submit component

---
+++ components/ui/form/FormSubmit.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { Button } from "@/components/ui/button";
+
+interface FormSubmitProps {
+  disabled?: boolean;
+  className?: string;
+  variant?:
+    | "default"
+    | "destructive"
+    | "outline"
+    | "secondary"
+    | "ghost"
+    | "link"
+    | "primary";
+}
+
+const {
+  className,
+  disabled,
+  variant = "primary",
+} = defineProps<FormSubmitProps>();
+</script>
+
+<template>
+  <Button
+    :disabled="disabled"
+    type="submit"
+    variant="primary"
+    size="sm"
+    :class="className"
+  >
+    <slot />
+  </Button>
+</template>
