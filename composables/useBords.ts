5a18c830cc4cbc641b94da56d83360db56972176
Tigran <harutyunyan.tigran1975@gmail.com>
2024-02-03 13:21:40 +0400

    created composable to get  boards list

---
+++ composables/useBords.ts
@@ -0,0 +1,40 @@
+import { useClerkProvide } from 'vue-clerk'
+
+export const useBoards = () => {
+    const { state } = useClerkProvide();
+    const isLoadingBoards = ref(false);
+
+    interface Iboard {
+        id: string;
+        title: string;
+        imageId: string;
+        imageThumbUrl: string;
+        imageFullUrl: string;
+        imageUserName: string;
+        imageLinkHTML: string;
+    }
+    const boards = ref<Iboard[]>();
+
+    const getBoards = async () => {
+
+        isLoadingBoards.value = true;
+        try {
+            const response = await $fetch(`/api/boards?orgId=${state.organization?.id}`, {
+                method: "get",
+            });
+
+            if (Array.isArray(response)) {
+                boards.value = response as Iboard[];
+            }
+        } catch (e) {
+
+        } finally {
+            isLoadingBoards.value = false
+        }
+    }
+    return {
+        getBoards,
+        isLoadingBoards,
+        boards
+    }
+}
