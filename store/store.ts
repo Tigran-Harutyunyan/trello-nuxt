ae56d02ed4c38134baa62e7c2ed4b45e14b5c590
Tigran <harutyunyan.tigran1975@gmail.com>
2024-02-08 00:47:52 +0400

    added methods to operated modals

--- store/store.ts
+++ store/store.ts
@@ -2,6 +2,20 @@ import { defineStore } from "pinia";
 
 export const useMainStore = defineStore("main", () => {
   const isSidebarOpen = ref(false);
+  const isCardModalOpen = ref(false);
+  const pendingCardId = ref(''); // Id of card to open modal with.
+
+  const setPendingCard = (id: string) => {
+    pendingCardId.value = id;
+  }
+
+  const onOpenCardModal = () => {
+    isCardModalOpen.value = true;
+  }
+
+  const onCloseCardModal = () => {
+    isCardModalOpen.value = false;
+  }
 
   const onOpenSidebar = () => {
     isSidebarOpen.value = true;
@@ -11,10 +25,19 @@ export const useMainStore = defineStore("main", () => {
     isSidebarOpen.value = false;
   }
 
+  const resetPendingCard = () => {
+    pendingCardId.value = '';
+  }
 
   return {
     isSidebarOpen,
+    isCardModalOpen,
+    pendingCardId,
     onOpenSidebar,
     onCloseSidebar,
+    onCloseCardModal,
+    onOpenCardModal,
+    setPendingCard,
+    resetPendingCard
   };
 });
