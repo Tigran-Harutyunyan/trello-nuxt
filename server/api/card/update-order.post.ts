43ef1e53b8760d474d7bd5347dbd6e5bb114ed22
Tigran <harutyunyan.tigran1975@gmail.com>
2024-02-07 10:08:59 +0400

    created end point to update board  card order

---
+++ server/api/card/update-order.post.ts
@@ -0,0 +1,51 @@
+import prisma from "../../../libs/prismadb";
+
+export default defineEventHandler(async (event) => {
+
+    const body = await readBody(event);
+
+    const {
+        items, orgId
+    } = body;
+
+    const { auth } = event.context;
+
+    if (!(auth.userId && orgId)) {
+        throw createError({
+            statusMessage: 'Unauthorized'
+        });
+    };
+
+    let updatedCards;
+
+    try {
+
+        const transaction = items.map((card) =>
+            prisma.card.update({
+                where: {
+                    id: card.id,
+                    list: {
+                        board: {
+                            orgId,
+                        },
+                    },
+                },
+                data: {
+                    order: card.order,
+                    listId: card.listId,
+                },
+            }),
+        );
+
+        updatedCards = await prisma.$transaction(transaction);
+
+        return updatedCards
+
+    } catch (error: any) {
+        throw createError({
+            statusCode: 500,
+            statusMessage: error
+        });
+    }
+
+});
