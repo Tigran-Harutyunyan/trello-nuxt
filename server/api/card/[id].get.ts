afe8ac68f42258a8e83fd2d5511c96060ea6fc2b
Tigran <harutyunyan.tigran1975@gmail.com>
2024-02-08 00:50:29 +0400

    added get cardendpoints

---
+++ server/api/card/[id].get.ts
@@ -0,0 +1,40 @@
+import prisma from "../../../libs/prismadb";
+
+export default defineEventHandler(async (event) => {
+
+    const { auth } = event.context;
+
+    const { id } = event.context.params;
+
+    const { orgId } = getQuery(event);
+
+    if (!(auth.userId && orgId)) return;
+
+    try {
+
+        const card = await prisma.card.findUnique({
+            where: {
+                id,
+                list: {
+                    board: {
+                        orgId: orgId as string,
+                    },
+                },
+            },
+            include: {
+                list: {
+                    select: {
+                        title: true,
+                    },
+                },
+            },
+        });
+
+        return card;
+
+    } catch (error: any) {
+        return {
+            error: "Failed to get the card."
+        }
+    }
+});
