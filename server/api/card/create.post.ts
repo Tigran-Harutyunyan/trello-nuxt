a5c43847f09b2b80addae3b90ca36678ed9ceda3
Tigran <harutyunyan.tigran1975@gmail.com>
2024-02-07 10:09:25 +0400

    created endpoint to create a board card

---
+++ server/api/card/create.post.ts
@@ -0,0 +1,74 @@
+import prisma from "../../../libs/prismadb";
+import { createAuditLog } from "@/lib/create-audit-log";
+import { ACTION, ENTITY_TYPE } from "@prisma/client";
+import { clerkClient } from 'h3-clerk'
+export default defineEventHandler(async (event) => {
+
+    const body = await readBody(event);
+
+    const {
+        title, orgId, listId
+    } = body;
+
+    const { auth } = event.context;
+
+    if (!(auth.userId && orgId)) {
+        throw createError({
+            statusMessage: 'Unauthorized'
+        });
+    };
+
+    const user = await clerkClient.users.getUser(auth.userId);
+
+
+    let card;
+
+    try {
+        const list = await prisma.list.findUnique({
+            where: {
+                id: listId,
+                board: {
+                    orgId,
+                },
+            },
+        });
+
+        if (!list) {
+            return {
+                error: "List not found",
+            };
+        }
+
+        const lastCard = await prisma.card.findFirst({
+            where: { listId },
+            orderBy: { order: "desc" },
+            select: { order: true },
+        });
+
+        const newOrder = lastCard ? lastCard.order + 1 : 1;
+
+        card = await prisma.card.create({
+            data: {
+                title,
+                listId,
+                order: newOrder,
+            },
+        });
+
+        await createAuditLog({
+            entityId: card.id,
+            entityTitle: card.title,
+            entityType: ENTITY_TYPE.CARD,
+            action: ACTION.CREATE,
+            orgId,
+            user
+        });
+
+        return card
+    } catch (error) {
+        return {
+            error: "Failed to create."
+        }
+    }
+
+});
