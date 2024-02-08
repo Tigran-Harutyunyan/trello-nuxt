844e1cfef5a050c287782410ddbc418aa0c25a22
Tigran <harutyunyan.tigran1975@gmail.com>
2024-02-08 13:49:10 +0400

    refactored create board component

--- server/api/board/index.post.ts
+++ server/api/board/index.post.ts
@@ -1,14 +1,38 @@
 import prisma from "../../../libs/prismadb";
+import { incrementAvailableCount, hasAvailableCount } from "@/lib/org-limit";
+import { checkSubscription } from "@/lib/subscription";
+import { createAuditLog } from "@/lib/create-audit-log";
+import { ACTION, ENTITY_TYPE } from "@prisma/client";
+import { clerkClient } from 'h3-clerk';
 
 export default defineEventHandler(async (event) => {
 
     const body = await readBody(event);
-
     const {
         orgId,
         title,
         image
     } = body;
+
+    const { auth } = event.context;
+
+    if (!(auth.userId && orgId)) return;
+
+    const user = await clerkClient.users.getUser(auth.userId);
+
+
+    const canCreate = await hasAvailableCount(orgId);
+    const isPro = await checkSubscription(orgId);
+
+
+    if (!canCreate && !isPro) {
+        return {
+            error: "You have reached your limit of free boards. Please upgrade to create more.",
+            local: true,
+            upgrade: true
+        }
+    }
+
 
     const [
         imageId,
@@ -20,17 +44,14 @@ export default defineEventHandler(async (event) => {
 
     if (!imageId || !imageThumbUrl || !imageFullUrl || !imageUserName || !imageLinkHTML) {
         return {
-            error: "Missing fields. Failed to create board."
+            error: "Missing fields. Failed to create board.",
+            local: true
         };
     }
 
-    const { auth } = event.context;
-
-    if (!(auth.userId && orgId)) return;
-
     try {
 
-        const boards = await prisma.board.create({
+        const board = await prisma.board.create({
             data: {
                 title,
                 orgId,
@@ -42,7 +63,19 @@ export default defineEventHandler(async (event) => {
             }
         })
 
-        return boards;
+        if (!isPro) {
+            await incrementAvailableCount(orgId);
+        }
+
+        await createAuditLog({
+            entityTitle: board.title,
+            entityId: board.id,
+            entityType: ENTITY_TYPE.BOARD,
+            action: ACTION.CREATE,
+            orgId,
+            user
+        });
+        return board;
 
     } catch (error: any) {
         throw createError({
