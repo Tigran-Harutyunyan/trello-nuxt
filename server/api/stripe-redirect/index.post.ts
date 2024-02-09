034dbf5a38f96a580f8fb7d9152b8d2da9e557d3
Tigran <harutyunyan.tigran1975@gmail.com>
2024-02-09 23:36:24 +0400

    integrated stripe

---
+++ server/api/stripe-redirect/index.post.ts
@@ -0,0 +1,84 @@
+import prisma from "../../../libs/prismadb";
+//import * as Stripe from 'stripe';
+import Stripe from "stripe";
+
+const STRIPE_API_KEY = useRuntimeConfig().stripeKey as string;
+const APP_URL = useRuntimeConfig().public.appUrl as string;
+
+const stripe = new Stripe(STRIPE_API_KEY, {
+    apiVersion: "2023-10-16",
+    typescript: true,
+});
+
+import { clerkClient } from 'h3-clerk';
+
+export default defineEventHandler(async (event) => {
+
+    const { auth } = event.context;
+
+    const user = await clerkClient.users.getUser(auth?.userId);
+
+    const { orgId } = getQuery(event)
+
+    if (!(auth && orgId)) return;
+
+    const settingsUrl = `${APP_URL}/organization/${orgId}`;
+
+    let url = "";
+
+    try {
+        const orgSubscription = await prisma.orgSubscription.findUnique({
+            where: {
+                orgId: orgId as string,
+            }
+        });
+
+
+        if (orgSubscription && orgSubscription.stripeCustomerId) {
+            const stripeSession = await stripe.billingPortal.sessions.create({
+                customer: orgSubscription.stripeCustomerId,
+                return_url: settingsUrl,
+            });
+
+            url = stripeSession.url;
+        } else {
+
+            const stripeSession = await stripe.checkout.sessions.create({
+                success_url: settingsUrl,
+                cancel_url: settingsUrl,
+                payment_method_types: ["card"],
+                mode: "subscription",
+                billing_address_collection: "auto",
+                customer_email: user.emailAddresses[0].emailAddress,
+                line_items: [
+                    {
+                        price_data: {
+                            currency: "USD",
+                            product_data: {
+                                name: "Taskify Pro",
+                                description: "Unlimited boards for your organization"
+                            },
+                            unit_amount: 2000,
+                            recurring: {
+                                interval: "month"
+                            },
+                        },
+                        quantity: 1,
+                    },
+                ],
+                metadata: {
+                    orgId,
+                },
+            });
+
+            url = stripeSession.url || "";
+        }
+        return { data: url };
+
+    } catch (error: any) {
+        return {
+            error: "Something went wrong!"
+        }
+    }
+
+});
---
+++ server/api/webhook/index.post.ts
@@ -0,0 +1,89 @@
+import prisma from "../../../libs/prismadb";
+import Stripe from "stripe";
+import { defineStripeWebhook } from '@fixers/nuxt-stripe/server'
+
+
+/**
+ * @param event - the H3 event
+ * @param stipe - the Stripe instance
+ * @param stripeEvent - the Stripe Webhook event
+ */
+
+const STRIPE_API_KEY = useRuntimeConfig().stripeKey as string;
+
+const webhookSecret = useRuntimeConfig().stripeWebhookSecret
+
+const stripe = new Stripe(STRIPE_API_KEY, {
+    apiVersion: "2023-10-16",
+    //typescript: true,
+});
+
+const webhookOptions = {
+    webhookSecret,
+    stripe,
+}
+
+// import { createStripeEvent } from '@fixers/nuxt-stripe/server'
+
+
+export default defineStripeWebhook(async ({ event, stripeEvent }) => {
+
+    if (!isMethod(event, ['POST'])) {
+        setResponseStatus(event, 400)
+
+        return { ok: false }
+    }
+
+    const session = stripeEvent.data.object as Stripe.Checkout.Session;
+
+    switch (stripeEvent.type) {
+        case 'checkout.session.completed': {
+            const subscription = await stripe.subscriptions.retrieve(
+                session.subscription as string
+            );
+            if (!session?.metadata?.orgId) {
+                throw createError({
+                    statusCode: 400,
+                    statusMessage: "Org ID is required"
+                });
+            }
+
+            await prisma.orgSubscription.create({
+                data: {
+                    orgId: session?.metadata?.orgId,
+                    stripeSubscriptionId: subscription.id,
+                    stripeCustomerId: subscription.customer as string,
+                    stripePriceId: subscription.items.data[0].price.id,
+                    stripeCurrentPeriodEnd: new Date(
+                        subscription.current_period_end * 1000
+                    ),
+                },
+            });
+
+        }
+        case 'invoice.payment_succeeded': {
+            const subscription = await stripe.subscriptions.retrieve(
+                session.subscription as string
+            );
+
+            await prisma.orgSubscription.update({
+                where: {
+                    stripeSubscriptionId: subscription.id,
+                },
+                data: {
+                    stripePriceId: subscription.items.data[0].price.id,
+                    stripeCurrentPeriodEnd: new Date(
+                        subscription.current_period_end * 1000,
+                    ),
+                },
+            });
+        }
+
+    }
+
+
+    return {
+        hello: 'world'
+    }
+
+}, webhookOptions); 
