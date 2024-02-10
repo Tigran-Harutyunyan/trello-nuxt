5325ad09935d9c079de4d204a9555bf9a60bb92b
Tigran <harutyunyan.tigran1975@gmail.com>
2024-02-10 14:28:53 +0400

    updated 404 page

--- pages/[...404].vue
+++ pages/[...404].vue
@@ -3,16 +3,32 @@ import { Button } from "~/components/ui/button";
 </script>
 
 <template>
-  <div class="mx-auto w-full flex items-center justify-center px-2 h-screen">
-    <div class="flex flex-col items-center my-auto">
-      <!-- <img src="/images/404.gif" class="w-40" /> -->
-
-      <p class="my-4 text-center font-bold">
-        Ooops! We can't seem to find the page you're looking for.
-      </p>
-      <NuxtLink to="/" class="mt-2 mx-auto">
-        <Button variant="primary">BACK TO HOMEPAGE</Button>
-      </NuxtLink>
+  <div
+    class="lg:px-24 lg:py-24 md:py-20 md:px-44 px-4 py-24 items-center flex justify-center flex-col-reverse lg:flex-row md:gap-28 gap-16"
+  >
+    <div class="xl:pt-24 w-full xl:w-1/2 relative pb-12 lg:pb-0">
+      <div class="relative">
+        <div class="absolute">
+          <div class="">
+            <h1 class="my-2 text-gray-800 font-bold text-2xl">
+              Looks like you've found the doorway to the great nothing
+            </h1>
+            <p class="my-2 text-gray-800">
+              Sorry about that! Please visit our hompage to get where you need
+              to go.
+            </p>
+            <NuxtLink to="/" class="mt-2 mx-auto">
+              <Button variant="primary">BACK TO HOMEPAGE</Button>
+            </NuxtLink>
+          </div>
+        </div>
+        <div>
+          <img src="https://i.ibb.co/G9DC8S0/404-2.png" />
+        </div>
+      </div>
+    </div>
+    <div>
+      <img src="https://i.ibb.co/ck1SGFJ/Group.png" />
     </div>
   </div>
 </template>
