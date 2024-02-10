1158aa54f3814a374c457abd099427622aa0fc4a
Tigran <harutyunyan.tigran1975@gmail.com>
2024-02-10 14:29:54 +0400

    switched to ClientOnly component

--- components/dashboard/Navbar.vue
+++ components/dashboard/Navbar.vue
@@ -32,52 +32,56 @@ const route = useRoute();
 </script>
 
 <template>
-  <nav
-    class="fixed z-50 top-0 px-4 w-full h-14 border-b shadow-sm bg-white flex items-center"
-  >
-    <ClientOnly>
+  <ClientOnly>
+    <nav
+      class="fixed z-50 top-0 px-4 w-full h-14 border-b shadow-sm bg-white flex items-center"
+    >
       <MobileSidebar />
-    </ClientOnly>
 
-    <div class="flex items-center gap-x-4">
-      <div class="hidden md:flex">
-        <Logo />
-      </div>
-      <FormPopover align="start" side="bottom" :sideOffset="18">
+      <div class="flex items-center gap-x-4">
+        <div class="hidden md:flex">
+          <Logo />
+        </div>
+        <FormPopover align="start" side="bottom" :sideOffset="18">
+          <Button
+            size="sm"
+            variant="primary"
+            class="rounded-sm hidden md:block h-auto py-1.5 px-2"
+          >
+            Create
+          </Button>
+        </FormPopover>
+
+        <FormPopover>
+          <Button
+            variant="primary"
+            size="sm"
+            class="rounded-sm block md:hidden"
+          >
+            <Plus class="h-4 w-4" />
+          </Button>
+        </FormPopover>
+
         <Button
           size="sm"
           variant="primary"
           class="rounded-sm hidden md:block h-auto py-1.5 px-2"
+          @click="router.push(`/organization/${state.organization?.id}`)"
+          v-if="route.name === 'board-id'"
         >
-          Create
+          Boards
         </Button>
-      </FormPopover>
-
-      <FormPopover>
-        <Button variant="primary" size="sm" class="rounded-sm block md:hidden">
-          <Plus class="h-4 w-4" />
-        </Button>
-      </FormPopover>
-
-      <Button
-        size="sm"
-        variant="primary"
-        class="rounded-sm hidden md:block h-auto py-1.5 px-2"
-        @click="router.push(`/organization/${state.organization?.id}`)"
-        v-if="route.name === 'board-id'"
-      >
-        Boards
-      </Button>
-    </div>
-    <div class="ml-auto flex items-center gap-x-2">
-      <OrganizationSwitcher
-        hidePersonal
-        :afterCreateOrganizationUrl="(org) => `/organization/${org.id}`"
-        :afterSelectOrganizationUrl="(org) => `/organization/${org.id}`"
-        afterLeaveOrganizationUrl="/select-org"
-        :appearance="switcherAppearance"
-      />
-      <UserButton afterSignOutUrl="/" :appearance="buttonAppearance" />
-    </div>
-  </nav>
+      </div>
+      <div class="ml-auto flex items-center gap-x-2">
+        <OrganizationSwitcher
+          hidePersonal
+          :afterCreateOrganizationUrl="(org) => `/organization/${org.id}`"
+          :afterSelectOrganizationUrl="(org) => `/organization/${org.id}`"
+          afterLeaveOrganizationUrl="/select-org"
+          :appearance="switcherAppearance"
+        />
+        <UserButton afterSignOutUrl="/" :appearance="buttonAppearance" />
+      </div>
+    </nav>
+  </ClientOnly>
 </template>
